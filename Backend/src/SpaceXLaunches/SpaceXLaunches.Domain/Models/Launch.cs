70d6533e8ab18bd5256be02fea04a112cd9ba61a
Moslem Hadi <moslem.hady@gmail.com>
2023-04-26 21:32:57 +0330

    clean the model

--- Backend/src/SpaceXLaunches/SpaceXLaunches.Domain/Models/Launch.cs
+++ Backend/src/SpaceXLaunches/SpaceXLaunches.Domain/Models/Launch.cs
@@ -1,156 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
-namespace SpaceXLaunches.Domain.Models
+namespace SpaceXLaunches.Domain.Models;
+
+//Version 3 model
+public class Launch
 {
-    //Version 3 model
-    public class Launch
-    {
-        public int FlightNumber { get; set; }
-        public string MissionName { get; set; }
-        //public List<object> MissionId { get; set; }
-        public bool Upcoming { get; set; }
-        public string LaunchYear { get; set; }
-        public int LaunchDateUnix { get; set; }
-        public DateTime LaunchDateUtc { get; set; }
-        public DateTime LaunchDateLocal { get; set; }
-        public bool IsTentative { get; set; }
-        //public string TentativeMaxPrecision { get; set; }
-        //public bool Tbd { get; set; }
-        //public int LaunchWindow { get; set; }
-        public Rocket Rocket { get; set; }
-        public List<string> Ships { get; set; }
-        public Telemetry Telemetry { get; set; }
-        public LaunchSite LaunchSite { get; set; }
-        public bool LaunchSuccess { get; set; }
-        public LaunchFailureDetails LaunchFailureDetails { get; set; }
-        public Links Links { get; set; }
-        public string Details { get; set; }
-        public DateTime StaticFireDateUtc { get; set; }
-        public int StaticFireDateUnix { get; set; }
-        public Timeline Timeline { get; set; }
-        public object Crew { get; set; }
-    }
+    [JsonPropertyName("flight_number")]
+    public int FlightNumber { get; set; }
 
-    public class Core
-    {
-        public string CoreSerial { get; set; }
-        public int Flight { get; set; }
-        public object Block { get; set; }
-        public bool Gridfins { get; set; }
-        public bool Legs { get; set; }
-        public bool Reused { get; set; }
-        public object LandSuccess { get; set; }
-        public bool LandingIntent { get; set; }
-        public object LandingType { get; set; }
-        public object LandingVehicle { get; set; }
-    }
-
-    public class Fairings
-    {
-        public bool Reused { get; set; }
-        public bool RecoveryAttempt { get; set; }
-        public bool Recovered { get; set; }
-        public object Ship { get; set; }
-    }
-
-    public class FirstStage
-    {
-        public List<Core> Cores { get; set; }
-    }
-
-    public class LaunchFailureDetails
-    {
-        public int Time { get; set; }
-        public object Altitude { get; set; }
-        public string Reason { get; set; }
-    }
-
-    public class LaunchSite
-    {
-        public string SiteId { get; set; }
-        public string SiteName { get; set; }
-        public string SiteNameLong { get; set; }
-    }
-
-    public class Links
-    {
-        public string MissionPatch { get; set; }
-        public string MissionPatchSmall { get; set; }
-        public object RedditCampaign { get; set; }
-        public object RedditLaunch { get; set; }
-        public object RedditRecovery { get; set; }
-        public object RedditMedia { get; set; }
-        public object Presskit { get; set; }
-        public string ArticleLink { get; set; }
-        public string Wikipedia { get; set; }
-        public string VideoLink { get; set; }
-        public string YoutubeId { get; set; }
-        public List<object> FlickrImages { get; set; }
-    }
-
-    public class OrbitParams
-    {
-        public string ReferenceSystem { get; set; }
-        public string Regime { get; set; }
-        public object Longitude { get; set; }
-        public object SemiMajorAxisKm { get; set; }
-        public object Eccentricity { get; set; }
-        public int PeriapsisKm { get; set; }
-        public int ApoapsisKm { get; set; }
-        public int InclinationDeg { get; set; }
-        public object PeriodMin { get; set; }
-        public object LifespanYears { get; set; }
-        public object Epoch { get; set; }
-        public object MeanMotion { get; set; }
-        public object Raan { get; set; }
-        public object ArgOfPericenter { get; set; }
-        public object MeanAnomaly { get; set; }
-    }
-
-    public class Payload
-    {
-        public string PayloadId { get; set; }
-        public List<object> NoradId { get; set; }
-        public bool Reused { get; set; }
-        public List<string> Customers { get; set; }
-        public string Nationality { get; set; }
-        public string Manufacturer { get; set; }
-        public string PayloadType { get; set; }
-        public int PayloadMassKg { get; set; }
-        public int PayloadMassLbs { get; set; }
-        public string Orbit { get; set; }
-        public OrbitParams OrbitParams { get; set; }
-    }
-
-    public class Rocket
-    {
-        public string RocketId { get; set; }
-        public string RocketName { get; set; }
-        public string RocketType { get; set; }
-        public FirstStage FirstStage { get; set; }
-        public SecondStage SecondStage { get; set; }
-        public Fairings Fairings { get; set; }
-    }
-    public class SecondStage
-    {
-        public int Block { get; set; }
-        public List<Payload> Payloads { get; set; }
-    }
-
-    public class Telemetry
-    {
-        public object FlightClub { get; set; }
-    }
-
-    public class Timeline
-    {
-        public int WebcastLiftoff { get; set; }
-    }
+    [JsonPropertyName("mission_name")]
+    public string MissionName { get; set; }
 
 
+    [JsonPropertyName("launch_year")]
+    public string LaunchYear { get; set; }
 
+    [JsonPropertyName("launch_date_unix")]
+    public int LaunchDateUnix { get; set; }
+
+    [JsonPropertyName("launch_date_utc")]
+    public DateTime LaunchDateUtc { get; set; }
+
+    [JsonPropertyName("launch_date_local")]
+    public DateTime LaunchDateLocal { get; set; }
+
+    [JsonPropertyName("rocket")]
+    public Rocket Rocket { get; set; }
+
+    [JsonPropertyName("launch_site")]
+    public LaunchSite LaunchSite { get; set; }
+
+    [JsonPropertyName("launch_success")]
+    public bool LaunchSuccess { get; set; }
+
+    [JsonPropertyName("launch_failure_details")]
+    public LaunchFailureDetails LaunchFailureDetails { get; set; }
+
+    [JsonPropertyName("links")]
+    public Links Links { get; set; }
+
+    [JsonPropertyName("details")]
+    public string Details { get; set; }
+
+    [JsonPropertyName("upcoming")]
+    public bool Upcoming { get; set; }
+
+    [JsonPropertyName("crew")]
+    public object Crew { get; set; }
+}
+public class LaunchFailureDetails
+{
+    [JsonPropertyName("time")]
+    public int Time { get; set; }
+
+    [JsonPropertyName("reason")]
+    public string Reason { get; set; }
+}
+
+public class LaunchSite
+{
+    [JsonPropertyName("site_id")]
+    public string SiteId { get; set; }
+
+    [JsonPropertyName("site_name")]
+    public string SiteName { get; set; }
+
+    [JsonPropertyName("site_name_long")]
+    public string SiteNameLong { get; set; }
+}
+
+public class Links
+{
+    [JsonPropertyName("mission_patch")]
+    public string MissionPatch { get; set; }
+
+    [JsonPropertyName("mission_patch_small")]
+    public string MissionPatchSmall { get; set; }
+
+    [JsonPropertyName("reddit_campaign")]
+    public object RedditCampaign { get; set; }
+
+    [JsonPropertyName("reddit_launch")]
+    public object RedditLaunch { get; set; }
+
+    [JsonPropertyName("reddit_recovery")]
+    public object RedditRecovery { get; set; }
+
+    [JsonPropertyName("reddit_media")]
+    public object RedditMedia { get; set; }
+
+    [JsonPropertyName("presskit")]
+    public object Presskit { get; set; }
+
+    [JsonPropertyName("article_link")]
+    public string ArticleLink { get; set; }
+
+    [JsonPropertyName("wikipedia")]
+    public string Wikipedia { get; set; }
+
+    [JsonPropertyName("video_link")]
+    public string VideoLink { get; set; }
+
+    [JsonPropertyName("youtube_id")]
+    public string YoutubeId { get; set; }
+
+    [JsonPropertyName("flickr_images")]
+    public List<object> FlickrImages { get; set; }
+}
+public class Rocket
+{
+    [JsonPropertyName("rocket_id")]
+    public string RocketId { get; set; }
+
+    [JsonPropertyName("rocket_name")]
+    public string RocketName { get; set; }
+
+    [JsonPropertyName("rocket_type")]
+    public string RocketType { get; set; }
 }
